228d1fc3aa4d119fae9f4252e08c05270bf6b19b
Arpita395 <karpita395@gmail.com>
2026-03-19 00:17:54 +0530

    Implement user authentication and profile management features

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
 // Generate JWT token
-const generateTojen= (id)=> {
+const generateToken= (id)=> {
     return jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE || "7d",
     })
@@ -11,3 +11,44 @@ const generateTojen= (id)=> {
 // @desc Register new user
 // @route POST /api/auth/register
 // @access Public
+export const register= async(req, res, next)=> {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+// @desc Login user
+// @route POST /api/auth/login
+// @access Public
+export const login= async(req, res, next) => {
+
+}
+
+// @desc Get user profile
+// @route POST /api/auth/profile
+// @access Private
+export const getProfile= async(req, res, next) => {
+
+}
+
+// @desc Update user profile
+// @route POST /api/auth/profile
+// @access Private
+export const updateProfile= async(req, res, next) => {
+
+}
+
+// @desc Change password 
+// @route POST /api/auth/chane-password
+// @access Private
+export const changePassword= async(req, res, next) => {
+
+}
+
+
+
+
+
+
---
+++ backend/models/Flashcard.js
@@ -0,0 +1,44 @@
+import mongoose, { mongo } from 'mongoose'
+
+const flashcardSchema= new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    documentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Document",
+        required: true,
+    },
+    cards: [{
+        questions: {type: String, required: true},
+        answer: {type: String, required: true},
+        difficulty: {
+            type: String,
+            enum: ["easy", "medium", "hard"],
+            default: "medium",
+        },
+        lastReviewed: {
+            type: Date,
+            default: null,
+        },
+        reviewCount: {
+            type: Number,
+            default: 0,
+        },
+        isStarred: {
+            type: Boolean,
+            default: false,
+        }
+    }]  
+
+}, {
+    timestamps: true
+})
+
+flashcardSchema.index({userId: 1, document: 1})
+
+const Flashcard= mongoose.model("Flashcard", flashcard)
+
+export default Flashcard
